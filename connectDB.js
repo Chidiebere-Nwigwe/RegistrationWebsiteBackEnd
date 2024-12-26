401a736aecb8ff2ab839d4c8821a0f6dbbe03f13
Chidiebere Nwigwe <chidonnwigz@gmail.com>
2024-12-26 13:35:51 -0700

    updated connection string

--- connectDB.js
+++ connectDB.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const dbURI = 'mongodb+srv://netninja:test1234@cluster0.wojwcap.mongodb.net/registration-form?retryWrites=true&w=majority&appName=Cluster0';
+// const dbURI = 'mongodb+srv://netninja:test1234@cluster0.wojwcap.mongodb.net/registration-form?retryWrites=true&w=majority&appName=Cluster0';
+const dbURI = 'mongodb+srv://chidiebere:test1234@cluster0.xpbsb.mongodb.net/'
 
 async function connectToMongoDB() {
     try {
